4da65f0ce5695d1fbddd50a7b03610c6f6cdcb86
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2014-04-20 17:02:15 +0200

    Add save brightness window.

---
+++ brightness-setter/BSSaveBrightnessWindowController.m
@@ -0,0 +1,33 @@
+//
+//  BSSaveBrightnessWindowController.m
+//  brightness-setter
+//
+//  Created by Enrico Maria Crisostomo on 20/04/14.
+//  Copyright (c) 2014 Enrico Maria Crisostomo. All rights reserved.
+//
+
+#import "BSSaveBrightnessWindowController.h"
+
+@interface BSSaveBrightnessWindowController ()
+
+@end
+
+@implementation BSSaveBrightnessWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
