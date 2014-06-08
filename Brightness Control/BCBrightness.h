2902fd819f20eea2a67e7907998d43ef4b305ad8
Enrico M. Crisostomo <enrico.m.crisostomo@gmail.com>
2014-06-08 14:21:31 +0200

    Add class to store brightness values.

---
+++ Brightness Control/BCBrightness.h
@@ -0,0 +1,16 @@
+//
+//  BCBrightness.h
+//  Brightness Control
+//
+//  Created by Enrico Maria Crisostomo on 07/06/14.
+//  Copyright (c) 2014 Enrico Maria Crisostomo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BCBrightness : NSObject
+
+@property (copy) NSString *name;
+@property (copy) NSString *value;
+
+@end
---
+++ Brightness Control/BCBrightness.m
@@ -0,0 +1,13 @@
+//
+//  BCBrightness.m
+//  Brightness Control
+//
+//  Created by Enrico Maria Crisostomo on 07/06/14.
+//  Copyright (c) 2014 Enrico Maria Crisostomo. All rights reserved.
+//
+
+#import "BCBrightness.h"
+
+@implementation BCBrightness
+
+@end
